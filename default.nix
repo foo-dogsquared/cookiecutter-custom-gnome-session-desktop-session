8a99d1e6c66b9628701a7fc89d162875f68ca5e3
Gabriel Arazas <foodogsquared@foodogsquared.one>
2023-09-02 16:26:27 +0800

    project: create the Nix package

---
+++ default.nix
@@ -0,0 +1,34 @@
+{ stdenv
+, lib
+, meson
+, ninja
+, pkg-config
+, makeWrapper
+, gnome
+}:
+
+stdenv.mkDerivation rec {
+  pname = "{{cookiecutter.workflow_name}}-custom-desktop-session";
+  version = "2023-08-21";
+
+  src = ./.;
+  nativeBuildInputs = [
+    meson
+    ninja
+    pkg-config
+    makeWrapper
+  ];
+
+  passthru.providedSessions = [ "{{cookiecutter.workflow_name}}" ];
+
+  postInstall = ''
+    wrapProgram "$out/libexec/{{cookiecutter.workflow_name}}-session" \
+      --prefix PATH : "${lib.makeBinPath [ gnome.gnome-session ]}"
+  '';
+
+  meta = with lib; {
+    description = "{{cookiecutter.workflow_description}}";
+    license = licenses.gpl3Plus;
+    platforms = platforms.linux;
+  };
+}
